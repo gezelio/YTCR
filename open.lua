7720d0ed85d194480b6a72d7ea607333c352d61e
Tommerty <86270372+tommerty@users.noreply.github.com>
2023-06-15 12:59:16 +0100

    Update open.lua

--- open.lua
+++ open.lua
@@ -1,8 +1,8 @@
 -- Define the URL to open
 local url = "https://ytcr.gezel.io"
 
--- Define the countdown duration in seconds
-local countdown_duration = 5
+-- Define the delay duration in seconds
+local delay_duration = 5
 
 -- Detect the operating system to determine the browser command
 local os_name = string.lower(package.config:sub(1, 1)) == '\\' and 'windows' or 'unix'
@@ -16,16 +16,14 @@ local browser_commands = {
 -- Get the browser command based on the operating system
 local browser_command = browser_commands[os_name]
 
--- Function to display the countdown
-local function countdown(seconds)
-  for i = seconds, 1, -1 do
-    print(i)
-    os.execute("timeout 1 >nul")
-  end
+-- Function to wait for a specific number of seconds
+local function delay(seconds)
+  local cmd = os_name == 'windows' and 'ping 127.0.0.1 -n ' .. seconds + 1 .. ' >nul' or 'sleep ' .. seconds
+  os.execute(cmd)
 end
 
--- Display the countdown
-countdown(countdown_duration)
+-- Wait for the specified delay duration
+delay(delay_duration)
 
 -- Open the URL in a new tab of the default browser
 local full_command = string.format(browser_command, url)
